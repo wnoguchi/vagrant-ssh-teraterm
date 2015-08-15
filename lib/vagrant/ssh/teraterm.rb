cddcb6e4b701750a369291361bd9e5e504e41450
Wataru Noguchi <wnoguchi.0727@gmail.com>
2015-08-15 16:39:39 +0900

    Initial commit.

---
+++ lib/vagrant/ssh/teraterm.rb
@@ -0,0 +1,9 @@
+require "vagrant/ssh/teraterm/version"
+
+module Vagrant
+  module Ssh
+    module Teraterm
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/vagrant/ssh/teraterm/version.rb
@@ -0,0 +1,7 @@
+module Vagrant
+  module Ssh
+    module Teraterm
+      VERSION = "0.1.0"
+    end
+  end
+end
